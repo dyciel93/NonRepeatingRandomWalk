c32d21f97023ed13531833175188e4325fce8f73
William Choi <dyciel93@gmail.com>
2018-10-17 22:28:58 -0400

    The program has been divided into separate functions

--- test.py
+++ test.py
@@ -3,63 +3,97 @@
 The program "walks" towards a certain direction
 '''
 
-import random
+
 import numpy as np
 import matplotlib.pyplot as plt
+import tkinter as tk
 
-def main():
-    N = 50;
-    coords = np.random.rand(100, 2)
-    x = coords[:,0]
-    y = coords[:,1]
-    #The probability of going up, down, left, right
-    prob = [0.65, 0.35, 0.45, 0.55]
-    posx = [30]
-    posy = [30]
-    rand_location = np.random.rand(100,2)
-    z = rand_location[:, 0]
 
-    upp = prob[0] > rand_location[:, 0]
-    downp = prob[1] < rand_location[:, 0]
+def gen_prob():
+    prob_up = np.random.rand()
+    prob_down = 1 - prob_up
+    prob_right = np.random.rand()
+    prob_left = 1 - prob_right
+    a = [prob_up, prob_down, prob_right, prob_left]
+    return a
 
-    rightp = prob[2] > rand_location[:, 1]
-    leftp = prob[3] < rand_location[: ,1]
+def next_step(x_init, y_init):
+    steps = 1000
+    coords = np.random.rand(steps, 2)
+    prob = gen_prob()
+    up = prob[0] > coords[:,0]
+    down = prob[1] > coords[:,0]
+    right = prob[2] > coords[:,1]
+    left = prob[3] > coords[:,1]
+    x_pos = [x_init]
+    y_pos = [y_init]
+    max_vertical = 100
+    min_vertical = -100
+    max_horizontal = 100
+    min_horizontal = 100
 
-    dict = {(posx[-1], posy[-1]): True}
-    for uu, dd, rr, ll in zip(upp, downp, rightp, leftp):
-        up = uu and posy[-1] < 100
-        down = dd and posy[-1] > 0
-        right = rr and posx[-1] < 100
-        left = ll and posx[-1] > 0
-        new_x = posx[-1] - left + right
-        new_y = posy[-1] - down + up
+    for iu, id, ir, il in zip(up, down, right, left):
+        min_step = 0.4
+        max_step = 1
+        step_size = generate_randomval(min_step, max_step)
 
-        if (new_x, new_y) in dict:
+        step_up = iu and np.bool_(y_pos[-1] < max_vertical)
+        step_down = id and np.bool_(y_pos[-1]  > min_vertical)
+        step_right = ir and np.bool_(x_pos[-1] < max_vertical)
+        step_left = il and np.bool_(x_pos[-1] > min_vertical)
+        y_pos.append(y_pos[-1] + step_size[0]*step_up - step_size[1]*step_down)
+        x_pos.append(x_pos[-1] + step_size[2]*step_right - step_size[3]*step_left)
 
-        else:
-            posx.append(new_x)
-            posy.append(new_y)
-            dict[(new_x,new_y)] = True
 
-    x_vector = np.array(posx)
-    y_vector = np.array(posy)
-    print(x_vector.shape, y_vector.shape)
-    # new_array = np.concatenate((x_vector,y_vector),axis = 1)
-    plt.figure(1)
-    plt.scatter(x_vector, y_vector)
-    for i in range(x_vector.shape[0]):
-        plt.text(x_vector[i], y_vector[i], str(i))
+    return [x_pos, y_pos]
+
+def generate_graph(pos):
+    plt.figure("Random Steps")
+    plot = plt.scatter(pos[0],pos[1])
+    # Use the below code to show that
+    # for i in range(0, len(pos[0])):
+    #      plt.text(pos[0][i],pos[1][i],i)
+
+    plt.title("Non Repeating Random Step")
+    plt.xlabel("X Axis")
+    plt.ylabel('Y Axis')
     plt.show()
 
-    # plt.figure(1)
-    #
-    # plt.subplot(2,1,1)
-    # plt.scatter(x, y)
-    # plt.subplot(2,1,2)
-    # plt.plot(x,y)
-    # plt.show()
+def generate_randomval(min, max):
+    rand_1 = min + (max-min)*np.random.rand(4)
+    return rand_1
 
-def next_step(x_pos, y_pos)
-    probability = mobile
+class user_interf():
+    def __init__ (self):
+        self.root = tk.Tk()
+        frame = tk.Frame(root)
+        frame.pack()
+        startButton = tk.Button(frame, text = "start", fg="black", bg = "white") # TODO: Add the "command" keyword in there
+        endButton = tk.Button(frame, text = "end", fg = "white") #Finish this portion #TODO: Add the "command" keyword in there
+    #TODO: Create a generic function that combines two different functions
+    '''
+    def combine_funcs(*funcs):
+        def combined_func(*args, **kwargs):
+            for f in funcs:
+                f(*args, **kwargs)
+        return combined_func
+    '''
+
 if __name__ == "__main__":
-    main()
+    # x = np.bool_(True)
+    # y = True
+    # z = x and y
+    # print(z, type(x), type(y))
+    root = tk.Tk()
+    frame = tk.Frame(root)
+    frame.pack()
+    startButton = tk.Button(frame, text="Start", fg="black" , bg="white")
+    endButton = tk.Button(frame, text="End", fg="red", bg="white")
+    startButton.pack()
+    endButton.pack()
+
+
+    root.mainloop()
+
+    # position = next_step(1,2)
+    # generate_graph(position)
